73e791a1b38eac5ebe905d2c1a86778b0b291a34
Kevin Kraus <kevin.kraus@outlook.com>
2025-01-08 16:02:19 -0500

    unused ref

--- DocumentQuestionsFunction/HttpTriggerUploadFile.cs
+++ DocumentQuestionsFunction/HttpTriggerUploadFile.cs
@@ -17,7 +17,6 @@ using System.Threading.Tasks;
 namespace DocumentQuestions.Function
 {
    public class HttpTriggerUploadFile(
-      ILogger<HttpTriggerUploadFile> log, 
       IConfiguration config)
    {
       [Function("HttpTriggerUploadFile")]
